b95f79f610bf64ec8f3661de7e313066d92b5a46
mehmet <sayinmehmet47@gmail.com>
2022-08-29 12:13:55 +0200

    e2e test complete

--- test/auth.e2e-spec.ts
+++ test/auth.e2e-spec.ts
@@ -29,4 +29,22 @@ describe('Authentication System', () => {
         expect(response.body.email).toBe(email);
       });
   });
+  it('signup as a new user then get the currently logged in user', async () => {
+    const email = 'asd2f@gmail.com';
+
+    const res = await request(app.getHttpServer())
+      .post('/auth/signup')
+      .send({
+        email,
+        password: '12342',
+      })
+      .expect(201);
+
+    const cookie = res.get('Set-Cookie');
+    const { body } = await request(app.getHttpServer())
+      .get('/auth/whoami')
+      .set('Cookie', cookie)
+      .expect(200);
+    expect(body.email).toEqual(email);
+  });
 });
